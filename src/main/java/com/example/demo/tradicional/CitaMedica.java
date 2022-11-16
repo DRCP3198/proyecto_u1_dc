f66648538a5cd4997b604fcd97a69644a68abef1
Dillan Coloma <renatocoloma2010@gmail.com>
2022-11-15 19:10:25 -0500

    Gestion de dependencias tradicional Taller 4

--- src/main/java/com/example/demo/tradicional/CitaMedica.java
+++ src/main/java/com/example/demo/tradicional/CitaMedica.java
@@ -9,27 +9,56 @@ public class CitaMedica {
 	private LocalDateTime fechaAgenda;
 	private Medico medico;
 	private Paciente paciente;
-	private String ced_medico;
+	
 
 	public void agendar(String numero, LocalDateTime fechaCita, String nombreMedico, String cedMedico,
-			String nombrePaciente, String cedulaPaciente) {
+			String nombrePaciente, String cedulaPaciente, String tipo) {
 
 		this.numero=numero;
 		this.fechaCita=fechaCita;
+		this.fechaAgenda = LocalDateTime.now();
 		
-		Medico med = new Medico();
+		Medico medObjeto = new Medico();
+		medObjeto.setCedula(cedMedico);
+		medObjeto.setNombre(nombreMedico);
 		
-		med.setCedula(cedMedico);
-		med.setNombre(nombreMedico);
+		this.medico= medObjeto;
 		
-		this.medico= med;
+		//TE tercera edad y ninio N
+		if(tipo.equals("TE")) {
+			PacienteTerceraEdad pacienteTE= new PacienteTerceraEdad();
+			
+			pacienteTE.setCodigoIESS("1213232323");
+			pacienteTE.setTipo("TE");
+			
+			this.paciente = pacienteTE;
+			System.out.println("Paciente con descuento");
+		}else {
+			PacienteNinio pacienteNinio = new PacienteNinio();
+			pacienteNinio.setPesoNacimiento(5);
+			pacienteNinio.setTipo("N");
+			this.paciente=pacienteNinio;
+			System.out.println("Paciente ninio sin descuento");
+		}
 		
 		
-		Paciente pac = new Paciente();
-		pac.setCedula(cedulaPaciente);
-		pac.setNombre(nombrePaciente);
+		this.paciente.setNombre(nombrePaciente);
+		this.paciente.setCedula(cedulaPaciente);
 		
-		this.paciente = pac;
+		this.guardarCita(this);
+		
+	}
+
+	private void guardarCita(CitaMedica cita) {
+		
+		//Insertar en la base de datos, guardar cita
+		System.out.println(cita);
+	}
+	
+	@Override
+	public String toString() {
+		return "CitaMedica [numero=" + numero + ", fechaCita=" + fechaCita + ", fechaAgenda=" + fechaAgenda
+				+ ", medico=" + medico + ", paciente=" + paciente + "]";
 	}
 
 	// SET y GET
--- src/main/java/com/example/demo/tradicional/Paciente.java
+++ src/main/java/com/example/demo/tradicional/Paciente.java
@@ -5,17 +5,30 @@ public class Paciente {
 
 	private String nombre;
 	private String cedula;
-	public String getNombre() {
+	private String tipo; 	//Sirve para saber si es paciente niño o 3ra edad
+	
+
+	protected String getTipo() {
+		return tipo;
+	}
+	protected void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+	protected String getNombre() {
 		return nombre;
 	}
-	public void setNombre(String nombre) {
+	protected void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
-	public String getCedula() {
+	protected String getCedula() {
 		return cedula;
 	}
-	public void setCedula(String cedula) {
+	protected void setCedula(String cedula) {
 		this.cedula = cedula;
+	}
+	@Override
+	public String toString() {
+		return "Paciente [nombre=" + nombre + ", cedula=" + cedula + "]";
 	}
 	
 	
