d34942fda9b4a869187efeeeb5c6052a694c9c72
Dillan Coloma <renatocoloma2010@gmail.com>
2022-11-18 18:50:55 -0500

    Taller 4

--- src/main/java/com/example/demo/ProyectoU1DcApplication.java
+++ src/main/java/com/example/demo/ProyectoU1DcApplication.java
@@ -3,6 +3,8 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//Dillan Coloma
+//Taller4
 @SpringBootApplication
 public class ProyectoU1DcApplication {
 
