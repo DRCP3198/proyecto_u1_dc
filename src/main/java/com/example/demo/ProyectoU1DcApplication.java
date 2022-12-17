182d943bcb459005a628809cce2e7599dda1a8c2
Dillan Coloma <renatocoloma2010@gmail.com>
2022-12-16 19:40:56 -0500

    Taller14

--- src/main/java/com/example/demo/ProyectoU1DcApplication.java
+++ src/main/java/com/example/demo/ProyectoU1DcApplication.java
@@ -13,6 +13,11 @@ import com.example.demo.banco.modelo.CuentaBancaria;
 import com.example.demo.banco.modelo.Transferencia;
 import com.example.demo.banco.service.ICuentaBancariaService;
 import com.example.demo.banco.service.ITransferenciaService;
+import com.example.demo.ejercicio1.modelo.Propietario;
+import com.example.demo.ejercicio1.modelo.Vehiculo;
+import com.example.demo.ejercicio1.service.IMatriculaService;
+import com.example.demo.ejercicio1.service.IPropietarioService;
+import com.example.demo.ejercicio1.service.IVehiculoService;
 import com.example.demo.herencia.PacienteCancerH;
 import com.example.demo.herencia.PacienteTerceraEdadH;
 import com.example.demo.spring.boot.CitaMedicaSB;
@@ -28,12 +33,21 @@ import com.example.demo.spring.boot.PacienteTerceraEdadSB;
 @SpringBootApplication
 public class ProyectoU1DcApplication implements CommandLineRunner {
     
-	@Autowired
+	/*@Autowired
 	private ICuentaBancariaService bancariaService;
 	
 	@Qualifier("grande")
 	@Autowired
 	private ITransferenciaService iTransferenciaService;
+	*/
+	@Autowired
+	private IVehiculoService iVehiculoService;
+	
+	@Autowired
+	private IPropietarioService iPropietarioService;
+	
+	private IMatriculaService iMatriculaService;
+	
 	
 	public static void main(String[] args) {
 		SpringApplication.run(ProyectoU1DcApplication.class, args);
@@ -46,7 +60,34 @@ public class ProyectoU1DcApplication implements CommandLineRunner {
 		// TODO Auto-generated method stub
 		System.out.println("SPRING BOOT");
 		
-		CuentaBancaria cuenta1 = new CuentaBancaria();
+		//Opcion 1
+		Vehiculo vehi = new Vehiculo();
+		vehi.setMarca("Toyta");
+		vehi.setPlaca("PDF12654");
+		vehi.setPrecio(new BigDecimal(20000));
+		vehi.setTipo("P");
+		this.iVehiculoService.crear(vehi);
+		//Es una opcion
+		vehi.setPrecio(new BigDecimal(10000));
+		vehi.setMarca("Toyota");
+		this.iVehiculoService.modificar(vehi);
+		
+		//Opcion 2
+		
+		
+		Propietario propietario = new Propietario();
+		propietario.setApellido("Colon");
+		propietario.setCedula("154774566");
+		propietario.setFechaNacimiento(LocalDateTime.of(1978, 8,31,12,35));
+		propietario.setNombre("Edison");
+		 this.iPropietarioService.guardar(propietario);
+		
+		//Opcion 3
+		
+		
+		
+		
+		/*CuentaBancaria cuenta1 = new CuentaBancaria();
 		cuenta1.setNumero("1255485");
 		cuenta1.setTipo("A");
 		cuenta1.setTitular("Dillan Coloma");
@@ -77,7 +118,7 @@ public class ProyectoU1DcApplication implements CommandLineRunner {
 		System.out.println("REPORTE 2");
 		for(Transferencia t:this.iTransferenciaService.buscarReporte()) {
 			System.out.println(t);
-		}
+		}*/
 		
 		
 	}
